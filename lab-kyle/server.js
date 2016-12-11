94110e5a5476b5235331046caf8b274a189a9743
Kyle Winckler <kyle.winckler@gmail.com>
2016-12-11 12:13:26 -0800

    moved files to proper directories - created get and post routes for pokemon and added to server.js

--- lab-kyle/server.js
+++ lab-kyle/server.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
-const PORT = process.env.PORT || 3000;
+const Router = require('./lib/router');
 
-const server = module.exports = http.createServer(function(req, res) {});
+const PORT = process.env.PORT || 3000;
+const router = new Router();
+
+require('./routes/poke-routes')(router);
+
+const server = http.createServer(router.route);
 
 server.listen(PORT, function() {
   console.log(`Server started on port: ${PORT}`);
