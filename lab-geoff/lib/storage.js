9a8f3487448226b112c7da6b798256384f09762c
Geoffrey Cox <gcox3d@gmail.com>
2016-12-11 14:02:25 -0800

    created createItem method in storage.js

--- lab-geoff/lib/storage.js
+++ lab-geoff/lib/storage.js
@@ -1,2 +1,23 @@
-// create storage module that will store resources by their type and id
 'use strict';
+
+let bluebird = require('bluebird'); //maybe should be Promise here
+let fs = bluebird.promisifyAll(require('fs'), {suffix:'Prom'});
+
+// let storage = {}; //not sure here
+
+exports.createItem = function(schemaName, item) {
+  if(!schemaName) {
+    return Promise.reject(new Error('need schemaName'));
+  }
+  if(!item) {
+    return Promise.reject(new Error('need item'));
+  }
+  let json = JSON.stringify(item);
+  return fs.writeFileProm('path to file', json)
+    .then(() => {
+      item;
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+};
