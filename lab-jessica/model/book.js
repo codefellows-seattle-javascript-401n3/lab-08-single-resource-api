44444865a77fc7c41e6d7deeb68e4a19a8195d43
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-12-11 10:26:39 -0800

    Added some error handling

--- lab-jessica/model/book.js
+++ lab-jessica/model/book.js
@@ -3,6 +3,8 @@
 const uuid = require('node-uuid');
 
 const Book = function(title, author){
+  if (!title) throw new Error('title expected');
+  if (!author) throw new Error('author expected');
   this.id = uuid.v4();
   this.title = title;
   this.author = author;
