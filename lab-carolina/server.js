a917842fbd6a7ebb2e5f13933fbf9afcb49cdc3b
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-12-11 13:39:07 -0800

    server started

--- lab-carolina/server.js
+++ lab-carolina/server.js
@@ -1,13 +1,56 @@
+'use strict';
+
+// node modules
 const http = require('http');
-const url = require('url');
-const queryString = require('querystring');
+// npm modules
+// app modules
+const Song = require('./model/song.js');
+const Router = require('./lib/router.js');
+const storage = require('./lib/storage.js');
+// module constants
 const PORT = process.env.PORT || 3000;
-const parseBody = require('./lib/parsebody.js');
+const router = new Router();
+// module logic
 
-const server = http.createServer(function(req, res){
-
+router.get('/api/song', function(req, res) {
+  if (req.url.query.id) {
+    storage.fetchItem('song', req.url.query.id)
+    .then( song => {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify(song));
+      res.end();
+    })
+    .catch( err => {
+      console.error(err);
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('not found');
+      res.end();
+    });
+    return;
+  }
+  console.error(err);
+  res.writeHead(400, {'Content-Type': 'text/plain'});
+  res.write('bad request');
+  res.end();
 });
 
+router.post('/api/song', function(req,res) {
+  try {
+    var song = new Song(req.body.title, req.body.artist);
+    storage.createItem('song', song);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(song));
+    res.end();
+  } catch (err) {
+    console.error(err);
+    res.writeHead(400, {'Content-Type': 'text/plain'});
+    res.write('bad request');
+    res.end();
+  }
+});
+
+const server = http.createServer(router.route());
+
 server.listen(PORT, function(){
-  console.log('listening on port ' + PORT);
+  console.log('server started on port ', PORT);
 });
