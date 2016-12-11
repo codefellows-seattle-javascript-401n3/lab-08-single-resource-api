97eb02dad16b6361cbee3ed9d2e1b6c34dd23c83
Geoffrey Cox <gcox3d@gmail.com>
2016-12-11 10:19:59 -0800

    created part of router.js

--- lab-geoff/lib/parser.js
+++ lab-geoff/lib/parser.js
@@ -1,2 +1,7 @@
 // create a body parser for PUT and POST requests will parse the json body
 // create url parser that uses url and querystring modules to parse request urls
+'use strict';
+
+let url = require('url');
+let querystring = require('querystring');
+
--- lab-geoff/lib/router.js
+++ lab-geoff/lib/router.js
@@ -1,2 +1,16 @@
 // create a router constructor for GET, POST, PUT and DELETE requests
 // create a route for CREATE, READ and DELETE operations
+'use strict';
+
+function Router() {
+  this.routes = {
+    GET: {},
+    POST: {},
+    PUT: {},
+    DELETE: {},
+  };
+}
+
+Router.prototype.get = function(ENDPOINT, callback) {
+  this.routes.GET[ENPOINT] = callback;
+};
