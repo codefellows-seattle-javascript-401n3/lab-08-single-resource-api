0dcc234874d911084a3b9bdfb2cd7def6950b721
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-12-11 19:27:22 -0800

    tests passing

--- lab-carolina/lib/storage.js
+++ lab-carolina/lib/storage.js
@@ -7,7 +7,7 @@ module.exports = exports = {};
 exports.createItem = function(schemaName, item){
   // do error handling
   if (!schemaName) return Promise.reject(new Error('expected schemaName'));
-  if (!item) return Promise.reject(new Error('expected item'));
+  if (!item) return Promise.reject(new Error('expect item'));
 
   if(!storage[schemaName]) storage[schemaName] = {};
   storage[schemaName][item.id] = item;
