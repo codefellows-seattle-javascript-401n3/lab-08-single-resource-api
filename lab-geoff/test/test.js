9dda7dffec95f265a99d8138a0dacc0ed9f8e304
Geoffrey Cox <gcox3d@gmail.com>
2016-12-11 21:45:43 -0800

    wrote first test for GET

--- lab-geoff/test/test.js
+++ lab-geoff/test/test.js
@@ -10,12 +10,23 @@ describe('testing the routes for book api', function() {
   describe('testing POST request', function() {
     it('should add a book to storage', function(done) {
       request.post('http://localhost:3000/books')
-        .send({title: 'testTitle', author: 'testAuthor'})
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.body.title).to.equal('testTitle');
-          done();
-        });
+      .send({title: 'testTitle', author: 'testAuthor'})
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.title).to.equal('testTitle');
+        book = res.body;
+        done();
+      });
+    });
+  });
+  describe('testing GET request', function() {
+    it('should get a book from storage', function(done) {
+      request.get(`http://localhost:3000/books?id=${book.id}`)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.id).to.equal(book.id);
+        done();
+      });
     });
   });
 });
