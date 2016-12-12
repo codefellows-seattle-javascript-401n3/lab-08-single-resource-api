f065b971f7c62360bec9499002d3167e6c85234a
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-12-11 23:57:14 -0800

    working on lab9

--- lab-carolina/lib/storage.js
+++ lab-carolina/lib/storage.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const storage = {};
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
+// const storage = {};
 
 module.exports = exports = {};
 
@@ -9,35 +11,58 @@ exports.createItem = function(schemaName, item){
   if (!schemaName) return Promise.reject(new Error('expected schemaName'));
   if (!item) return Promise.reject(new Error('expect item'));
 
-  if(!storage[schemaName]) storage[schemaName] = {};
-  storage[schemaName][item.id] = item;
-  return Promise.resolve(item);
+  // if(!storage[schemaName]) storage[schemaName] = {};
+  // storage[schemaName][item.id] = item;
+  // return Promise.resolve(item);
+
+  let json = JSON.stringify(item);
+  return fs.writeFileProm('${__dirname}/../data/${schemaName}/${item.id}.json', json)
+  .then(() => item)
+  .catch(err => Promise.reject(err));
 };
 
-exports.fetchItem = function(schemaName, id){
-  // do error handling
-  return new Promise((resolve, reject) => {
-    if (!schemaName) return reject(new Error('expected schemaName'));
-    if (!id) return reject(new Error('expected id'));
 
-    var schema = storage[schemaName];
-    if(!schema) return reject(new Error('schema not found'));
-    var item = schema[id];
-    if(!item) return reject(new Error('item not found'));
-    resolve(item);
-  });
+exports.fetchItem = function(schemaName, id){
+  if(!schemaName) return Promise.reject(new Error('expected schemaName'));
+  if(!id) return Promise.reject(new Error('expected id'));
+
+  return fs.readFileProm('${__dirname}/../data/${schemaName}/${id}.json')
+  .then(data => {
+    try{
+      let item = JSON.parse(data.toString());
+      return item;
+    } catch(err){
+      return Promise.reject(err);
+    }
+  }).catch(err => Promise.reject(err));
+  // do error handling
+  // return new Promise((resolve, reject) => {
+  //   if (!schemaName) return reject(new Error('expected schemaName'));
+  //   if (!id) return reject(new Error('expected id'));
+  //
+  //   var schema = storage[schemaName];
+  //   if(!schema) return reject(new Error('schema not found'));
+  //   var item = schema[id];
+  //   if(!item) return reject(new Error('item not found'));
+  //   resolve(item);
+  // });
 };
 
 exports.deleteItem = function(schemaName, id){
-  return new Promise((resolve, reject) =>{
-    if(!schemaName) return reject(new Error('schema not found'));
-    if(!id) return reject(new Error('id not found'));
+  if(!schemaName) return Promise.reject(new Error('schema not found'));
+  if(!id) return Promise.reject(new Error('id not found'));
 
-    var schema = storage[schemaName];
-    if(!schema) return reject(new Error('schema not found'));
-    var item = schema[id];
-    if(!item) return reject(new Error('item not found'));
-    delete schema[id];
-    resolve();
-  });
+  return fs.unlinkProm('{__dirname}/../data/${schemaName}/${id}.json')
+  .catch(err => Promise.reject(err));
 };
+  // return new Promise((resolve, reject) =>{
+  //   if(!schemaName) return reject(new Error('schema not found'));
+  //   if(!id) return reject(new Error('id not found'));
+  //
+  //   var schema = storage[schemaName];
+  //   if(!schema) return reject(new Error('schema not found'));
+  //   var item = schema[id];
+  //   if(!item) return reject(new Error('item not found'));
+  //   delete schema[id];
+  //   resolve();
+  // });
