5155bbf8b524945cf5edd9f13ad65a6108e0187e
JESSICA VASQUEZ-SOLTERO <JMICHELLEVS@GMAIL.COM>
2016-12-12 14:26:27 -0800

    Create and fetch with simple persistence

--- lab-jessica/lib/storage.js
+++ lab-jessica/lib/storage.js
@@ -1,64 +1,26 @@
 'use strict';
 
-const del = require('del');
-const mkdirp = require('mkdirp');
 const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
+
+const storage = {};
 
 module.exports = exports = {};
 
 exports.createItem = function(schemaName, item) {
   if (!schemaName) return Promise.reject(new Error('expected schemaName'));
   if (!item) return Promise.reject(new Error('expected item'));
-  let json = JSON.stringify(item);
-  mkdirp(`${__dirname}/../data/${schemaName}/`, function() {
-    fs.writeFileProm(`${__dirname}/../data/${schemaName}/${item.id}.json`, json)
-    .then( () => item)
-    .catch( err => Promise.reject(err));
-  });
+
+  if(!storage[schemaName]) storage[schemaName] = {};
+  storage[schemaName][item.id] = item;
+  return Promise.resolve(item);
 };
 
 exports.fetchItem = function(schemaName, id) {
-  if (!schemaName) return Promise.reject(new Error('expected schemaName'));
-  if (!id) return Promise.reject(new Error('expected id'));
+  if(!schemaName) return Promise.reject(new Error('expected schemaName'));
+  if(!id) return Promise.reject(new Error('expected id'));
 
-  return fs.readFileProm(`${__dirname}/../data/${schemaName}/${id}.json`)
-  .then(data => {
-    try {
-      let item = JSON.parse(data.toString());
-      return item;
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  })
-  .catch(err => Promise.reject(err));
-};
-
-// exports.fetchAll = function(schemaName) {
-//   if (!schemaName) return Promise.reject(new Error('expected schemaName'));
-//
-//   fs.readdirProm(`${__dirname}/../data/${schemaName}`)
-//   .then(fileArr => {
-//     console.log(fileArr);
-//     // return fileArr;
-//     // Promise.resolve(fileArr);
-//   })
-//   .catch(err => Promise.reject(err));
-// };
-
-exports.deleteItem = function(schemaName, id) {
-  if (!schemaName) return Promise.reject(new Error('expected schemaName'));
-  if (!id) return Promise.reject(new Error('expected id'));
-
-  if(fs.existsSync(`${__dirname}/../data/${schemaName}/${id}.json`)) {
-    del([`${__dirname}/../data/${schemaName}/${id}.json`])
-    .then(paths => {
-      console.log('Deleted files and folders:\n', paths.join('\n'));
-    })
-    .catch(err => {
-      console.error(err);
-    });
-    return;
-  }
-  return Promise.reject(new Error('file path does not exist'));
+  if(!storage[schemaName]) return Promise. reject(new Error('schema not found'));
+  var item = storage[schemaName][id];
+  if(!item) return Promise.reject(new Error('item not found'));
+  return Promise.resolve(item);
 };
