f98704b2712d6bd9967ab9a59cddf721d0ea7c31
Geoffrey Cox <gcox3d@gmail.com>
2016-12-13 00:50:43 -0800

    began reworking fetch item function

--- lab-geoff/lib/storage.js
+++ lab-geoff/lib/storage.js
@@ -1,33 +1,27 @@
 'use strict';
 let del = require('del');
 let mkdirp = require('mkdirp');
+let Promise = require('bluebird');
+let fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
 
-let storage = {};
+// let storage = {};
+let storage = './../data/';
 
 exports.createItem = function(schemaName, item) {
   if(!storage[schemaName]) {
-    storage[schemaName] = {};
+    // storage[schemaName] = {};
+    console.log(storage);
+    console.log(schemaName);
+    console.log(storage[schemaName]);
   }
   storage[schemaName][item.id] = item;
   return Promise.resolve(item);
 };
 exports.fetchItem = function(schemaName, id) {
   return new Promise((resolve, reject) => {
-    if (!schemaName) {
-      return reject(new Error('need schemaName'));
-    }
-    if (!id) {
-      return reject(new Error('need id'));
-    }
-    let schema = storage[schemaName];
-    if (!schema) {
-      return reject(new Error('no schema found'));
-    }
-    let item = schema[id];
-    if (!item) {
-      return reject(new Error('no item found'));
-    }
-    resolve(item);
+    if (!schemaName) return reject(new Error('need schemaName'));
+    if (!id) return reject(new Error('need id'));
+    
   });
 };
 exports.deleteItem = function(schemaName, id) {
