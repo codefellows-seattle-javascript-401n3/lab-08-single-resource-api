7d1b22dbf7933910f4eca07f43d2be487f3576cb
Frank Iriarte <jurrish@gmail.com>
2016-12-12 17:07:03 -0800

    why broken

--- route/recipe-route.js
+++ route/recipe-route.js
@@ -1,3 +1,41 @@
 'use strict';
 
-const storage = 
+const storage = require('./storage/storage');
+const response = require('./lib/response');
+const recipe = require('./model/recipe');
+
+module.exports = function(router){
+  router.get('/api/recipe', function(req, res){
+    if(req.url.query.id){
+      storage.fetchItem('recipe', req.url.query.id)
+      .then(recipe => {
+        response.sendJSON(res, 200, recipe);
+      })
+      .catch(err => {
+        console.error(err);
+        response.sendText((res, 404, 'nope'));
+      });
+      return;
+    }
+    response.sendText(res, 400, 'bad request');
+  });
+
+  router.post('api/recipe', function(req, res) {
+    try {
+      var recipe = new Recipe(req.body.name, req.body.ingredients);
+      storage.createItem('recipe', recipe);
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      res.write(JSON.stringify(recipe));
+      res.end();
+    } catch (err) {
+      console.error(err);
+      res.writeHead(400, {
+        'Content-Type': 'text/plain',
+      });
+      res.write('bad request');
+      res.end();
+    }
+  });
+};
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const http = require('http');
-const Router = require('../lib/router');
+const Router = require('./lib/router');
 const PORT = process.env.PORT || 3000;
 const router = Router();
 
+//dependency injection (kinda like IIFE)
+require('./route/recipe-route')(router);
 const server = http.createServer(router.routes());
 
 //setup router.method logic. what's going on here? what does router.get take in as parameters? if resolved, what? if rejected, what?
-
-
-//dependency injection (kinda like IIFE) 
 
 server.listen(PORT, () => {
   console.log('serving port 3000');
