9e7e8fcbf37ee897184d05df09f7382100443f01
Geoffrey Cox <gcox3d@gmail.com>
2016-12-13 04:20:32 -0800

    fetch and delete item tests now passing

--- lab-geoff/lib/storage.js
+++ lab-geoff/lib/storage.js
@@ -1,11 +1,6 @@
 'use strict';
-let del = require('del');
-let mkdirp = require('mkdirp');
 let Promise = require('bluebird');
 let fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
-
-// let storage = {};
-let storage = '/../data/';
 
 exports.createItem = function(schemaName, item) {
   return new Promise((resolve, reject) => {
@@ -21,30 +16,25 @@ exports.createItem = function(schemaName, item) {
   });
 };
 exports.fetchItem = function(schemaName, id) {
-  return new Promise((resolve, reject) => {
-    if (!schemaName) return reject(new Error('need schemaName'));
-    if (!id) return reject(new Error('need id'));
-    return fs.readFileProm(`${__dirname}/../data/${schemaName}/${id}.json`)
-    .then(data => {
-      let item = JSON.parse(data.toString());
-      console.log(item);
-      resolve(item);
-    })
-    .catch(err => {
-      Promise.reject(err);
-    });
+  if (!schemaName) return Promise.reject(new Error('need schemaName'));
+  if (!id) return Promise.reject(new Error('need id'));
+  return fs.readFileProm(`${__dirname}/../data/${schemaName}/${id}.json`)
+  .then(data => {
+    let item = JSON.parse(data.toString());
+    return item;
+  })
+  .catch(err => {
+    Promise.reject(err);
   });
 };
 exports.deleteItem = function(schemaName, id) {
-  return new Promise((resolve, reject) => {
-    if (!schemaName) return reject(new Error('need schemaName'));
-    if (!id) return reject(new Error('need id'));
-    return fs.unlinkProm(`${__dirname}/../data/${schemaName}/${id}.json`)
-    .then(() => {
-      console.log('file deleted');
-    })
-    .catch(err => {
-      Promise.reject(err);
-    });
+  if (!schemaName) return Promise.reject(new Error('need schemaName'));
+  if (!id) return Promise.reject(new Error('need id'));
+  return fs.unlinkProm(`${__dirname}/../data/${schemaName}/${id}.json`)
+  .then(() => {
+    console.log('file deleted');
+  })
+  .catch(err => {
+    Promise.reject(err);
   });
 };
