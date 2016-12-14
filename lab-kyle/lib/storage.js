7aeb122da79433d34fea291fa63e2fe5d696ffd4
Kyle Winckler <kyle.winckler@gmail.com>
2016-12-13 16:24:27 -0800

    added file system persistance

--- lab-kyle/lib/storage.js
+++ lab-kyle/lib/storage.js
@@ -1,7 +1,8 @@
 let storage = {};
 
-let mkdirp = require('mkdirp');
 let path = require('path');
+const mkdirp = require('mkdirp');
+const del = require('del');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
 
@@ -12,31 +13,36 @@ exports.createItem = function(schemaName, item){
   if (!item) return Promise.reject(new Error('expected item'));
   if (!item.name && !item.color) return Promise.reject(new Error('item is not valid'));
 
-  // if (!storage[schemaName]) storage[schemaName] = {};
-  // storage[schemaName][item.id] = item;
-  // return Promise.resolve(item);
-
   let json = JSON.stringify(item);
-  return fs.writeFileProm(`../lab-kyle/data/${schemaName}/${item.id}.json`, json)
-    .then( () => item)
+  return fs.writeFileProm(`${__dirname}/../data/${schemaName}/${item.id}.json`, json)
     .catch( err => {
       if (err.code === 'ENOENT') {
         // make the directory
-        mkdrip('')
-
-        fs.writeFileProm
+        mkdirp(`${__dirname}/../data/${schemaName}`, function (err) {
+          if (err) console.error(err);
+          // then write to file
+          fs.writeFileProm(`${__dirname}/../data/${schemaName}/${item.id}.json`, json);
+        });
+      } else {
+        Promise.reject(err);
       }
-    }
+    });
 };
 
 exports.fetchItem = function(schemaName, id) {
   if (!schemaName) return Promise.reject(new Error('expected schema'));
   if (!id) return Promise.reject(new Error('expected id'));
 
-  if (!storage[schemaName]) return Promise.reject(new Error('schema does not exist'));
-  if (!storage[schemaName][id]) return Promise.reject(new Error('id does not exist'));
-
-  return Promise.resolve(storage[schemaName][id]);
+  return fs.readFileProm(`${__dirname}/../data/${schemaName}/${id}.json`)
+    .then( data => {
+      try {
+        let item = JSON.parse(data.toString());
+        return item;
+      } catch (err){
+        return Promise.reject(error);
+      }
+    })
+    .catch( err => Promise.reject(err));
 };
 
 exports.fetchAll = function(schemaName) {
@@ -51,7 +57,13 @@ exports.deleteItem = function(schemaName, id) {
   if (!schemaName) return Promise.reject(new Error('expected schema'));
   if (!id) return Promise.reject(new Error('expected id'));
 
-  if (!storage[schemaName]) return Promise.reject(new Error('schema does not exist'));
-  delete storage[schemaName][id];
-  return Promise.resolve();
+  // if (!storage[schemaName]) return Promise.reject(new Error('schema does not exist'));
+  // delete storage[schemaName][id];
+
+  // return Promise.resolve();
+  // del.sync(['public/assets/**', '!public/assets', '!public/assets/goat.png']);
+  return del([`${__dirname}/../data/${schemaName}/${id}.json`], {dryRun: true})
+            .then(paths => {
+              console.log('Files and folders that would be deleted:\n', paths.join('\n'));
+            });
 };
